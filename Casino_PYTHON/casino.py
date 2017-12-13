07cd4325bc3a3e6a1e434cdb607d63e5a8cbb000
Gosia <misbadu@gmail.com>
2017-12-13 23:18:19 +0100

    Random added to Casino

--- Casino_PYTHON/casino.py
+++ Casino_PYTHON/casino.py
@@ -1,11 +1,16 @@
 # coding=utf-8
+import random
+
 print "Welcome in the Casino"
 
-guess = raw_input("Now enter the secret number: ")
+secret_num = random.randint(0, 19)
+while True:
+    guess = int(raw_input("Enter the secret number (between 1 and 20): "))
+    if guess > secret_num:
+        print "The number is smaller than your guess. Try again."
+    elif guess < secret_num:
+        print "The number is bigger than your guess. Try again."
+    else:
+        print "Congratulations! You won this game!"
+        break
 
-secret = "25"
-
-if guess == secret:
-    print "Congratulations! You won this game!"
-else:
-    print "Not this time. Try again later."
